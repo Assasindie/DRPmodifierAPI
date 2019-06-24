858563606cd1f84556df079aa8562dc66ccddaf0
Assasindie <xmanbutler@gmail.com>
2019-06-24 11:15:53 +1000

    Add Cors
    localhost 😎

--- DRPmodifierAPI/Startup.cs
+++ DRPmodifierAPI/Startup.cs
@@ -20,11 +20,23 @@ namespace DRPmodifierAPI
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins("https://localhost:44340")
+                                       .AllowAnyHeader()
+                                       .AllowAnyMethod();
+                });
+            });
+
             services.AddControllers();
         }
 
@@ -40,6 +52,8 @@ namespace DRPmodifierAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseHttpsRedirection();
 
